c836eb9b39139c6d2fc5e0de561bd03dcb274178
Mikhail.Alifanau <mikhail.alifanau@itechart-group.com>
2022-05-28 21:43:14 +0400

    init commit

---
+++ src/main/java/com/malifanau/redisprtask/RedisPrTaskApplication.java
@@ -0,0 +1,13 @@
+package com.malifanau.redisprtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPrTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisPrTaskApplication.class, args);
+    }
+
+}
